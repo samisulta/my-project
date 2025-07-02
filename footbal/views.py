9bb5501c5d62815fab22541d820bedfffe68613a
rashidsarwarzada <sarwarzadar@gmai.com>
2025-07-02 03:46:18 -0700

    startup message has been  changed

--- footbal/views.py
+++ footbal/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('this is a demo change ...')
+    return HttpResponse('this is a demo change by Rashid ...')
 
